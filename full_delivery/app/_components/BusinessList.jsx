b7f6652f69b1af6b7a6f12bf2d2e5f03bb39ef74
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-06-24 23:33:54 +0200

    add styles components BusinessItem

--- full_delivery/app/_components/BusinessList.jsx
+++ full_delivery/app/_components/BusinessList.jsx
@@ -11,28 +11,34 @@ const BusinessList = () => {
     const [category, setCategory] = useState('all');
     const [businessList, setBusinessList] = useState([]);
 
+    const getBusinessList = (category_) => {
+        getBusiness(category_).then(resp => setBusinessList(resp?.restaurants));
+    }
+
     useEffect(() => {
         params && setCategory(params.get('category'));
         params && getBusinessList(params.get('category'));
     }, [params]);
 
-    const getBusinessList = (category_) => {
-        getBusiness(category_).then(resp => setBusinessList(resp?.restaurant));
-    }
 
-    //console.log(businessList)
+    console.log(businessList)
 
     return (
         <div className="mt-5">
             <h2 className="font-bold text-2xl">Popular {category} Restaurants</h2>
             <h2 className="font-bold text_primary">{businessList?.length}</h2>
 
-            <div>
-                {
-                    businessList?.map((restaurants, index) => (
+            <div className="grid grid-cols-1 
+                sm:grid-cols-2
+                md:grid-cols-3
+                lg:grid-cols-4
+                gap-7 mt-3
+            ">
+                {/* {
+                    businessList.map((restaurants, index) => (
                         <BussinessItem key={index} business={restaurants} />
                     ))
-                }
+                } */}
             </div>
         </div>
     )
