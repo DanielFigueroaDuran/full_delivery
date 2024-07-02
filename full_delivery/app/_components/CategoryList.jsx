d292c80f8fff8709ef765e2aca2f07a338aa3589
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-07-02 22:39:31 +0200

    add ScrollLeghtHandler

--- full_delivery/app/_components/CategoryList.jsx
+++ full_delivery/app/_components/CategoryList.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useRef, useState } from "react";
 import { getCategory } from "../_utils/GlobalApi";
 import Image from "next/image";
-import { ArrowRightCircle } from "lucide-react";
+import { ArrowLeftCircle, ArrowRightCircle } from "lucide-react";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 
@@ -35,8 +35,21 @@ const CategoryList = () => {
         }
     };
 
+    const ScrollLeghtHandler = () => {
+        if (listRef.current) {
+            listRef.current.scrollBy({
+                right: -100,
+                behavior: 'smooth'
+            })
+        }
+    };
+
     return (
         <div className="mt-10 relative">
+
+            <ArrowLeftCircle className="absolute -left-10 top-9 bg-gray-500 rounded-full text-white h-8 w-8 cursor-pointer"
+                onClick={() => ScrollRightHandler()} />
+
             <div className="flex gap-4 overflow-auto scrollbar-hide" ref={listRef}>
                 {
                     categoryList && categoryList.map((category, index) => (
@@ -48,7 +61,6 @@ const CategoryList = () => {
                             className={`flex flex-col items-center gap-2 border p-3 rounded-xl min-w-28 hover:border-primary hover:bg-orange-50 cursor-pointer group ${selectedCategory == category.slug && 'text-primary border-primary  bg-orange-50'}`}
                         // as="valid-value"
                         >
-
 
                             <Image
                                 src={category.icon?.url}
